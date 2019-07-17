fb849fb0bc2660fe93009c9a4301a6bc9e712677
Gokhan Sagirlar <gokhan.sagirlar1@ibm.com>
2019-07-17 17:04:53 +0100

    some fork len arrangements

--- Chain Quality/Bitcoin/Init.m
+++ Chain Quality/Bitcoin/Init.m
@@ -10,6 +10,9 @@ global alphaPower gammaRatio maxForkLen lowerBoundReward upperBoundReward;
 
 alphaGroup = [0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45];
 
+gammaGroup = [0.0 0.5];
+
+
 gammaRatio = 0;
 lowerBoundRewardZero = [];
 upperBoundRewardZero = [];
@@ -61,3 +64,130 @@ end
 %     converge. Even 160 is not always enough.
 
 
+
+for gammaRatio = gammaGroup
+    lowerBoundReward140= [];
+    upperBoundReward140 = [];
+    for alphaPower = alphaGroup
+        if alphaPower <= 0.4
+            maxForkLen = 70;
+        else
+            maxForkLen = 140;
+        end
+        InitStates;
+        SolveMDP;
+        lowerBoundReward140 = [lowerBoundReward140 lowerBoundReward];
+        upperBoundReward140 = [upperBoundReward140 upperBoundReward];
+    end
+end
+
+gammaRatio = 0;
+lowerBoundReward120= [];
+upperBoundReward120 = [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 60;
+    else
+        maxForkLen = 120;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward120 = [lowerBoundReward120 lowerBoundReward];
+    upperBoundReward120 = [upperBoundReward120 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward100= [];
+upperBoundReward100 = [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 50;
+    else
+        maxForkLen = 100;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward100 = [lowerBoundReward100 lowerBoundReward];
+    upperBoundReward100 = [upperBoundReward100 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward80= [];
+upperBoundReward80 = [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 40;
+    else
+        maxForkLen = 80;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward80 = [lowerBoundReward80 lowerBoundReward];
+    upperBoundReward80 = [upperBoundReward80 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward60= [];
+upperBoundReward60= [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 30;
+    else
+        maxForkLen = 60;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward60 = [lowerBoundReward60 lowerBoundReward];
+    upperBoundReward60 = [upperBoundReward60 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward40= [];
+upperBoundReward40= [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 20;
+    else
+        maxForkLen = 40;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward40 = [lowerBoundReward40 lowerBoundReward];
+    upperBoundReward40 = [upperBoundReward40 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward20= [];
+upperBoundReward20 = [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 10;
+    else
+        maxForkLen = 20;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward20 = [lowerBoundReward20 lowerBoundReward];
+    upperBoundReward20 = [upperBoundReward20 upperBoundReward];
+end
+
+
+gammaRatio = 0;
+lowerBoundReward10= [];
+upperBoundReward10 = [];
+for alphaPower = alphaGroup
+    if alphaPower <= 0.4
+        maxForkLen = 5;
+    else
+        maxForkLen = 10;
+    end
+    InitStates;
+    SolveMDP;
+    lowerBoundReward10 = [lowerBoundReward10 lowerBoundReward];
+    upperBoundReward10 = [upperBoundReward10 upperBoundReward];
+end
